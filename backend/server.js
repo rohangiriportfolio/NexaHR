c99f7b3686923d723670ecb75377a2e7566846b9
Rohan Giri <rohangiri.portfolio@gmail.com>
2026-03-31 02:36:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -287,4 +287,4 @@ app.put('/api/employee/change-password', authMiddleware(['employee']), async (re
 
 // ─── Start Server ─────────────────────────────────────────────────────────────
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on Port:${PORT}`));
